c93ed5e25e0e1be8524ee73ac9109ef1ca9a1697
kiurem66 <kiurem66@gmail.con>
2022-12-19 15:17:56 +0100

    Cleaned day10/Part2

--- day10/Part2.java
+++ day10/Part2.java
@@ -7,37 +7,21 @@ public class Part2 {
         Scanner scanner = new Scanner(System.in);
         int x = 1;
         int cycle = 1;
-        int sum=0;
 
         while(scanner.hasNextLine()){
             draw(cycle, x);
-            boolean check = false;
             String[] line = scanner.nextLine().split(" ");
             cycle++;
-
-            if(cycle==20 || cycle==60 || cycle==100 || cycle==140 || cycle==180 || cycle==220){
-                sum+=cycle*x;
-                check=true;
-            }
-
             if(line[0].equals("addx")){
                 draw(cycle, x);
                 x += Integer.valueOf(line[1]);
                 cycle++;
-            }
-
-            
-            
-            if(cycle==20 || cycle==60 || cycle==100 || cycle==140 || cycle==180 || cycle==220){
-                if(check) continue;
-                sum+=cycle*x;
             }
         }
         scanner.close();
     }   
 
     static void draw(int cycle, int x){
-        //int line = (cycle-1)/40;
         int offset = (cycle-1)%40;
 
         if(Math.abs(offset-x) <= 1)System.out.print("#");
